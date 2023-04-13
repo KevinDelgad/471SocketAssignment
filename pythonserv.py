23a9fbd94db7c1aa233212d69877ad3971e039a9
Kevin Delgado <kevindel891@gmail.com>
2023-04-12 20:48:52 -0700

    Initial Server

---
+++ pythonserv.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = int(sys.argv[1])
+
+welcomeSocket.bind(('', port))
+welcomeSocket.listen(1)
+
+print("Server is ready to recieve!")
+
+while(True):
+    connectionSocket, addr = welcomeSocket.accept()
+    connectionSocket.close()
