7ca7fb3d52e385e5e7c36d507c90466608bedcd7
Kevin Delgado <kevindel891@gmail.com>
2023-04-15 14:02:18 -0700

    add basic functionality for recive and list commands

--- pythonserv.py
+++ pythonserv.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import subprocess
 
 welcomeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,32 +20,46 @@ while(True):
     usersInput = connectionSocket.recv(1024).decode()
     
     splitCommand = usersInput.split()
-
     command = splitCommand[0]
-    targetFile = splitCommand[1]
 
     if(command in acceptableRequests):
         print("Valid Request")
     else:
         print("Request not found")
 
-    fileData = 0
-    fileObj = open(targetFile, "r")
+    if(command == 'get'):
+        targetFile = splitCommand[1]
+        fileData = 0
+        fileObj = open(targetFile, "r")
 
-    fileData = fileObj.read()
-    dataSize = str(len(fileData))
+        fileData = fileObj.read()
+        dataSize = str(len(fileData))
 
-    while(len(dataSize) < 10):
-        dataSize = '0' + dataSize
+        while(len(dataSize) < 10):
+            dataSize = '0' + dataSize
 
-    fileData = dataSize + " | File Content: " + fileData
+        fileData = dataSize + " | File Content: " + fileData
 
-    numSent = 0
+        numSent = 0
 
-    while(numSent < len(fileData)):
-        numSent += connectionSocket.send(fileData[numSent:].encode())
+        while(numSent < len(fileData)):
+            numSent += connectionSocket.send(fileData[numSent:].encode())
 
-    fileObj.close() 
+        fileObj.close() 
+
+    if(command == 'put'):
+        recievedData = connectionSocket.recv(1024)
+
+        print("From Server:", recievedData.decode())
+
+    if(command == "ls"):
+        listedData = subprocess.getstatusoutput('ls')[1]
+            
+        listedDataSize = len(listedData)
+        listedDataNumSent = 0
+
+        while(listedDataNumSent < listedDataSize):
+            listedDataNumSent += connectionSocket.send(listedData[listedDataNumSent:].encode())
 
     connectionSocket.close()
     break
