9b425177f2487a42b2b0975de43278aa4edb8e60
Kevin Delgado <kevindel891@gmail.com>
2023-04-26 15:45:55 -0700

    add ephermeral port stuff

--- cli.py
+++ cli.py
@@ -17,13 +17,24 @@ acceptableRequests = ['get', 'put', 'ls', 'quit']
 
 while(True):
     command = input("ftp> ").strip()
-    connectionSocket.send(command.encode())
+
+    tempSocket = socket(AF_INET, SOCK_STREAM)
+    tempSocket.bind(('', 0))
+    tempSocket.listen(1)
+    
+    metaData = str(tempSocket.getsockname()[1]) + " " + command
+
+
+    connectionSocket.send(metaData.encode())
+
+    tempSendSocket, addr = tempSocket.accept()
+
+
     splitCommand = command.split()
     command = splitCommand[0]
-
     if command == 'get':
         targetFile = splitCommand[1]
-        fileData = connectionSocket.recv(1024).decode()
+        fileData = tempSendSocket.recv(1024).decode()
         
         with open(targetFile, "w") as fileObj:
             fileObj.write(fileData)
@@ -32,6 +43,7 @@ while(True):
 
     if command == 'put':
         fileData = 0
+        targetFile = splitCommand[1]
         fileObj = open(targetFile, "r")
 
         fileData = fileObj.read()
@@ -45,7 +57,7 @@ while(True):
         numSent = 0
 
         while(numSent < len(fileData)):
-            numSent += connectionSocket.send(fileData[numSent:].encode())
+            numSent += tempSendSocket.send(fileData[numSent:].encode())
 
         fileObj.close() 
 
@@ -53,21 +65,19 @@ while(True):
         print("Successful Put Request")
 
     if command == 'ls':
-        connectionSocket.sendall('ls'.encode())
+        listedData = b''
+        while True:
+            data = tempSendSocket.recv(bufferSize)
+            if b'\0' in data:
+                data = data.replace(b'\0', b'')
+                listedData += data
+                break
+            elif not data:
+                break
+            else:
+                listedData += data
 
-    listedData = b''
-    while True:
-        data = connectionSocket.recv(bufferSize)
-        if b'\0' in data:
-            data = data.replace(b'\0', b'')
-            listedData += data
-            break
-        elif not data:
-            break
-        else:
-            listedData += data
-
-    print(listedData.decode())
+        print(listedData.decode())
  
     if command == 'quit':
             connectionSocket.sendall(b'quit')
@@ -76,5 +86,5 @@ while(True):
             break
     
     
-    
-
+    tempSocket.close()
+    tempSendSocket.close()
--- pythonserv.py
+++ pythonserv.py
@@ -33,82 +33,87 @@ welcomeSocket.listen(1)
 
 print("Server is ready to recieve!")
 
-data = "test Data"
 acceptableRequests = ['get', 'put', 'ls', 'quit']
-
-
 
 while(True):
     connectionSocket, addr = welcomeSocket.accept()
     print("Connection Recieved!")
-    usersInput = connectionSocket.recv(1024).decode()
-    
-    splitCommand = usersInput.split()
-    command = splitCommand[0]
+    while(True):
+        receivedPayload = connectionSocket.recv(1024).decode()
+        ePort = receivedPayload.split()[0]
+        usersInput = receivedPayload.split()[1:]
+        command = usersInput[0]
 
-    if(command == 'get'):
-        targetFile = splitCommand[1]
-        fileData = 0
-        fileObj = open(targetFile, "r")
+        tempSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tempSocket.bind(('', 0))
 
-        fileData = fileObj.read()
-        dataSize = str(len(fileData))
+        tempSocket.connect(('localhost', int(ePort)))
 
-        while(len(dataSize) < 10):
-            dataSize = '0' + dataSize
+        if(command == 'get'):
+            targetFile = usersInput[1]
+            fileData = 0
+            fileObj = open(targetFile, "r")
 
-        fileData = dataSize + " | File Content: " + fileData
+            fileData = fileObj.read()
+            dataSize = str(len(fileData))
 
-        numSent = 0
+            while(len(dataSize) < 10):
+                dataSize = '0' + dataSize
 
-        while(numSent < len(fileData)):
-            numSent += connectionSocket.send(fileData[numSent:].encode())
+            fileData = dataSize + " | File Content: " + fileData
 
-        fileObj.close() 
+            numSent = 0
 
-    if(command == 'put'):
-        # The buffer to all data received from the
-        # the client.
-        recvFileData = ""
-        
-        # The temporary buffer to store the received
-        # data.
-        recvBuff = ""
-        
-        # The size of the incoming file
-        fileSize = 0	
-        
-        # The buffer containing the file size
-        fileSizeBuff = ""
-        
-        # Receive the first 10 bytes indicating the
-        # size of the file
-        fileSizeBuff = recvAll(connectionSocket, 10) 
-        # Get the file size
-        fileSize = int(fileSizeBuff)
-        # Get the file data
-        recvFileData = recvAll(connectionSocket, fileSize)
-        print(recvFileData)
+            while(numSent < len(fileData)):
+                numSent += tempSocket.send(fileData[numSent:].encode())
 
-    if command == "ls":
-        listedData = subprocess.getstatusoutput('ls')[1]
+            fileObj.close() 
 
-        listedDataSize = len(listedData)
-        listedDataNumSent = 0
+        if(command == 'put'):
+            # The buffer to all data received from the
+            # the client.
+            recvFileData = ""
+            
+            # The temporary buffer to store the received
+            # data.
+            recvBuff = ""
+            
+            # The size of the incoming file
+            fileSize = 0	
+            
+            # The buffer containing the file size
+            fileSizeBuff = ""
+            
+            # Receive the first 10 bytes indicating the
+            # size of the file
+            fileSizeBuff = recvAll(tempSocket, 10) 
+            # Get the file size
+            fileSize = int(fileSizeBuff)
+            # Get the file data
+            recvFileData = recvAll(tempSocket, fileSize)
+            print(recvFileData)
 
-        while listedDataNumSent < listedDataSize:
-            listedDataNumSent += connectionSocket.send(listedData[listedDataNumSent:].encode())
+        if command == "ls":
+            listedData = subprocess.getstatusoutput('ls')[1]
 
-        # Had to add sending null byte after transmitting all the listed data to let client know it ended
-        connectionSocket.send(b'\0')
+            listedDataSize = len(listedData)
+            listedDataNumSent = 0
+
+            while listedDataNumSent < listedDataSize:
+                listedDataNumSent += tempSocket.send(listedData[listedDataNumSent:].encode())
+
+            # Had to add sending null byte after transmitting all the listed data to let client know it ended
+            tempSocket.send(b'\0')
 
 
-    if(command in acceptableRequests):
-        connectionSocket.send(("Sucessful Command").encode())
-    else:
-        connectionSocket.send(("Unsucessful Command").encode())
+        if(command in acceptableRequests):
+            tempSocket.send(("Sucessful Command").encode())
+        else:
+            tempSocket.send(("Unsucessful Command").encode())
 
-    if(command == "quit"):
-        connectionSocket.close()
-        break
+        if(command == "quit"):
+            connectionSocket.close()
+            break
 
+    tempSocket.close()
+    break
